738f2528ac3bfca3bb87d1bf8529e99f1dec95a2
FuelAir <116159276+FuelAir@users.noreply.github.com>
2021-04-05 12:44:33 +0000

    panels

--- hashUtils.c
+++ hashUtils.c
@@ -6,4 +6,5 @@
 #include "hashUtils.h"
 
 void cpyhash(unsigned char * dst, unsigned char * org){
-    for(int i = 0; i<SHA256_DIGEST_LENGTH; i+
+    for(int i = 0; i<SHA256_DIGEST_LENGTH; i++){
+  
