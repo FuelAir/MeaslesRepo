1611219d085597d0f367b2564527604f5894cda6
FuelAir <116159276+FuelAir@users.noreply.github.com>
2021-06-06 07:41:18 +0000

    Use latest primogenitor

--- hashUtils.c
+++ hashUtils.c
@@ -18,4 +18,4 @@ void printhash(unsigned char * hash){
     printf("\n");
 }
 
-//Carrega o vetor com os valores de cada carteira da blo
+//Carrega o vetor com os valores de cada carteira da blockch
