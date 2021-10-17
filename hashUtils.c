9415e3326d06203365eb1cc8e717ca9a7855f64a
FuelAir <116159276+FuelAir@users.noreply.github.com>
2021-10-17 19:05:46 +0000

    Clarify instructions

--- hashUtils.c
+++ hashUtils.c
@@ -36,4 +36,4 @@ BlocoMinerado searchBlock(int index){
 
     FILE * pFile = fopen("data/blockchain.dat","rb");
     fseek(pFile, -1024L, SEEK_END);
-    int sz = ftell(pFile)/sizeof(BlocoMine
+    int sz = ftell(pFile)/sizeof(BlocoMinerado
