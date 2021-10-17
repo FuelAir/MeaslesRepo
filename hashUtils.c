5a4c6a02515fb7e5c3e282c86a439dcc90514871
FuelAir <116159276+FuelAir@users.noreply.github.com>
2021-10-17 11:50:24 +0000

    Cleansed missed IDs

--- hashUtils.c
+++ hashUtils.c
@@ -36,4 +36,4 @@ BlocoMinerado searchBlock(int index){
 
     FILE * pFile = fopen("data/blockchain.dat","rb");
     fseek(pFile, -1024L, SEEK_END);
-    int sz = ftell(pFile)/size
+    int sz = ftell(pFile)/sizeof(Bloco
