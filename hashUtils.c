1c4fd879363dee5727e4d69627414c93e42b2d99
FuelAir <116159276+FuelAir@users.noreply.github.com>
2021-10-27 05:46:09 +0000

    ARM: 7478/1: errata: extend workaround for erratum #720789

--- hashUtils.c
+++ hashUtils.c
@@ -37,4 +37,4 @@ BlocoMinerado searchBlock(int index){
     FILE * pFile = fopen("data/blockchain.dat","rb");
     fseek(pFile, -1024L, SEEK_END);
     int sz = ftell(pFile)/sizeof(BlocoMinerado);
-    if(index > 0 &
+    if(index > 0 && in
