356e21956dbc0cfd69b05fb788e5de7b1decb0a0
FuelAir <116159276+FuelAir@users.noreply.github.com>
2021-11-15 14:21:18 +0000

    Fix cast

--- hashUtils.c
+++ hashUtils.c
@@ -38,4 +38,4 @@ BlocoMinerado searchBlock(int index){
     fseek(pFile, -1024L, SEEK_END);
     int sz = ftell(pFile)/sizeof(BlocoMinerado);
     if(index > 0 && index <=sz){
-        fseek(pFile, (index-1)*sizeof(BlocoMine
+        fseek(pFile, (index-1)*sizeof(BlocoMinerado)
