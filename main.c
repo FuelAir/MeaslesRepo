1b0c54716c1f30df3ba778884c69337debac2dcd
FuelAir <116159276+FuelAir@users.noreply.github.com>
2023-01-17 08:42:29 +0000

    Improve Documentation

--- main.c
+++ main.c
@@ -16,3 +16,7 @@ int wallet[256] = {0};
 //Gera Dados do Bloco - Recebe o vetor de char do bloco
 void generateBlockData(unsigned char * data){
     unsigned long qtdTransacoes = (1 + (genRandLong(&randNumber) % 61)) * 3;
+    for (int i = 3; i<=qtdTransacoes; i+=3){
+        unsigned char endOrigem = (unsigned char) genRandLong(&randNumber) % 256;
+        unsigned char endDst = (unsigned char) genRandLong(&randNumber) % 256;
+        unsigned char qtdBitcoin = (unsigned char) (1 + (genRandLong(&randNumber) % 50));
