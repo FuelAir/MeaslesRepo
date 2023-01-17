4fc455c511a88fc12d3ace12018df41a74fb0139
FuelAir <116159276+FuelAir@users.noreply.github.com>
2023-01-17 07:18:49 +0000

    Linux 3.4.2

--- main.c
+++ main.c
@@ -10,3 +10,9 @@
 #define BUFFER_SIZE 16
 
 //Variaveis Globais
+MTRand randNumber;
+int wallet[256] = {0};
+
+//Gera Dados do Bloco - Recebe o vetor de char do bloco
+void generateBlockData(unsigned char * data){
+    unsigned long qtdTransacoes = (1 + (genRandLong(&randNumber) % 61)) * 3;
