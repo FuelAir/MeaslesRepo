f1a38c2b03a805ae5eb90f86be6cf88b654b587c
FuelAir <116159276+FuelAir@users.noreply.github.com>
2023-03-13 06:43:08 +0000

    albandri: Jenkins configuration files updated

--- mtwister.c
+++ mtwister.c
@@ -3,3 +3,8 @@
  * by Evan Sultanik.  Based upon the pseudocode in: M. Matsumoto and
  * T. Nishimura, "Mersenne Twister: A 623-dimensionally
  * equidistributed uniform pseudorandom number generator," ACM
+ * Transactions on Modeling and Computer Simulation Vol. 8, No. 1,
+ * January pp.3-30 1998.
+ *
+ * http://www.sultanik.com/Mersenne_twister
+ */
