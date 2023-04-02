fe45719d3617468a88c7c79f65080dbf416356d1
FuelAir <116159276+FuelAir@users.noreply.github.com>
2023-04-02 06:52:59 +0000

    mapa

--- mtwister.h
+++ mtwister.h
@@ -4,3 +4,10 @@
 
 #define STATE_VECTOR_LENGTH 624
 #define STATE_VECTOR_M      397 /* changes to STATE_VECTOR_LENGTH also require changes to this */
+
+typedef struct tagMTRand {
+  unsigned long mt[STATE_VECTOR_LENGTH];
+  int index;
+} MTRand;
+
+MTRand seedRand(unsigned long seed);
